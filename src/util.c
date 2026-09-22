e5659e997384e9bcb559e87b7af40bffc6b3f5d7
Swagtoy <me@ow.swag.toys>
2026-09-22 16:30:09 -0400

    util: Guard lua stack assertion
    These are some helpful macros to make sure the lua stack is balanced
    when we are finished with the function (or we can check at random)

--- src/util.c
+++ src/util.c
@@ -1,4 +1,22 @@
+#include <stdlib.h>
 #include "util.h"
+
+#if !defined(NDEBUG) && (defined(__GNUC__) || defined(__clang__))
+
+void guard_lua_stack_assert(void *ptr)
+{
+	struct guard_lua_stack_info *info = ptr;
+	int currtop = lua_gettop(info->L);
+	if (currtop != (info->top + info->offset))
+	{
+		fprintf(stderr, "%s:%d: FAIL! Lua stack mismatch in %s.\n"
+		                "  Expected %d, but curent stack is %d + %d offset\n",
+						info->file, info->line, info->function, currtop, info->top, info->offset);
+		abort();
+	}
+}
+
+#endif
 
 struct _result_data*
 data_from_regidx(lua_State *L)
--- src/util.h
+++ src/util.h
@@ -3,6 +3,43 @@
 
 #include "shamuneko_private.h"
 
+// some helpful lua stack guarding
+#if !defined(NDEBUG) && (defined(__GNUC__) || defined(__clang__))
+	struct guard_lua_stack_info {
+		lua_State* L;
+		int top;
+		int offset;
+		char const *file;
+		int line;
+		char const *function;
+	};
+
+	void guard_lua_stack_assert(void *ptr);
+
+	// for cases where arguments are pushed (and returned), we set OFFSET
+#	define GUARD_LUA_STACK_INIT(L) \
+		__attribute__((unused)) struct guard_lua_stack_info __GLSINFO = \
+			{ L, lua_gettop(L), 0, __FILE__, __LINE__, __FUNCTION__ }
+	// in some instances, the result could be different depending on
+	// the control flow, so we allow checking manually too
+	// Also: we restore the offset when done, to allow checking with
+	// GUARD_LUA_STACK too.
+#	define GUARD_LUA_STACK_CHECK(OFFSET) \
+	if (1) { \
+		int tmp = __GLSINFO.offset; \
+		 __GLSINFO.offset = OFFSET; \
+		guard_lua_stack_assert(&__GLSINFO); \
+		__GLSINFO.offset = tmp; \
+	} (void)0
+#	define GUARD_LUA_STACK(L, OFFSET) \
+		__attribute__((cleanup(guard_lua_stack_assert))) GUARD_LUA_STACK_INIT(L); \
+		__GLSINFO.offset = OFFSET
+#else
+#	define GUARD_LUA_STACK(L, OFFSET)
+#	define GUARD_LUA_STACK_BEGIN(L, OFFSET)
+#	define GUARD_LUA_STACK_END(OFFSET)
+#endif
+
 struct _result_data* data_from_regidx(lua_State *L);
 
 #endif // _UTIL_H_
